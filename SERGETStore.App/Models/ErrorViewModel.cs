9abb78ed726d32813dd55bcb86d1696af441b0a2
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-12-24 14:45:47 -0300

    criado projeto MVC

---
+++ SERGETStore.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SERGETStore.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
