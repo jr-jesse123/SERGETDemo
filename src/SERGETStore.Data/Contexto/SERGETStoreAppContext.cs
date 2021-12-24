7a4a2eaf25d5601f7e737123027ffaddb9f632b4
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-12-24 15:03:01 -0300

    Criado DBContext

---
+++ src/SERGETStore.Data/Contexto/SERGETStoreAppContext.cs
@@ -0,0 +1,22 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERGETStore.Data.Contexto
+{
+    public class SERGETStoreAppContext : DbContext
+    {
+        public SERGETStoreAppContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
