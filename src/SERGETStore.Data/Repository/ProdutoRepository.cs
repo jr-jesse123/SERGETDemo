8b3c72c3be6eb8cb979699a2c94b2842bd16bfda
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-12-24 16:44:15 -0300

    repositório abstrato e produtorepository implementados

---
+++ src/SERGETStore.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,40 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using SERGETStore.Business.Interfaces;
+using SERGETStore.Data.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERGETStore.Data.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        readonly IQueryable<Produto> _produtosComFornecedores; 
+
+        public ProdutoRepository(SERGETStoreAppContext db) : base(db)
+        {
+            _produtosComFornecedores = Db.Produtos.AsNoTracking().Include(p => p.Fornecedor);
+        }
+
+        //TODO: TESTAR
+        public async Task<Produto> ObterProdutoE_Fornecedor(Guid fornecedorId)
+        {
+            //return await Db.Produtos.AsNoTracking().Include(p => p.Fornecedor)
+            //    .FirstOrDefaultAsync(p => p.Id == fornecedorId);
+            return await  _produtosComFornecedores.FirstOrDefaultAsync(p => p.Id == fornecedorId);
+        }
+        //TODO: TESTAR
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await _produtosComFornecedores.OrderBy(p => p.Nome).ToListAsync();
+        }
+        //TODO: TESTAR
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+    }
+}
---
+++ src/SERGETStore.Data/Repository/Repository.cs
@@ -0,0 +1,60 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using SERGETStore.Business.Interfaces;
+using SERGETStore.Data.Contexto;
+using System.Linq.Expressions;
+
+namespace SERGETStore.Data.Repository;
+
+
+public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+{
+    protected readonly SERGETStoreAppContext Db;
+    readonly DbSet<TEntity> DbSet;
+    protected Repository(SERGETStoreAppContext db)
+    {
+        Db = db;
+        DbSet = db.Set<TEntity>();  
+    }
+
+    public async Task Adiciontar(TEntity entity)
+    {
+        DbSet.Add(entity);
+        await SaveChanges();
+    }
+
+    public async Task Atualizar(TEntity entity)
+    {
+        DbSet.Update(entity);
+        await SaveChanges();
+    }
+
+    public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+    {
+         return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+    }
+
+    public async Task<TEntity> ObterPorId(Guid id)
+    {
+       return await DbSet.Where(e => e.Id == id).FirstOrDefaultAsync(); 
+    }
+
+    public async Task Remover(Guid id)
+    {
+        
+        var entity = new TEntity { Id = id };
+        DbSet.Remove(entity);
+        await SaveChanges();
+
+    }
+
+    public async Task<int> SaveChanges()
+    {
+        return await Db.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        Db?.Dispose();
+    }
+}
