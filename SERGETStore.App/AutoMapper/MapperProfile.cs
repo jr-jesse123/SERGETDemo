49bcde7f1ff9016fd48f140cd49c362e44be9ceb
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-12-24 21:02:50 -0300

    Criado mapeamento entre entitdades de negócio e viewmodels

---
+++ SERGETStore.App/AutoMapper/MapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DevIO.Business.Models;
+using SERGETStore.App.ViewModels;
+
+namespace SERGETStore.App.AutoMapper
+{
+    public class MapperProfile : Profile
+    {
+        //TODO: TESTAR PROFILES
+        public MapperProfile()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
