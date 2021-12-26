ba3fc29e078801868ad6dd0b66f99b954792d3af
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-12-26 11:41:24 -0300

    Implementados Ajustes de globalização

--- SERGETStore.App/Program.cs
+++ SERGETStore.App/Program.cs
@@ -5,6 +5,10 @@ using SERGETStore.Data.Contexto;
 using SERGETStore.App.Extentions;
 using SERGETStore.Business.Interfaces;
 using SERGETStore.Data.Repository;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,7 +35,27 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<UserDbContext>();
 
-builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddControllersWithViews(o =>
+{
+    o.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((_, _) => "O valor preenchido é inválido para este campo.");
+    o.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor((_) => "Este campo precisa ser preenchido.");
+    o.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(() => "Este campo precisa ser preenchido");
+    o.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que o body na requisição ñao esteja vazio");
+    o.ModelBindingMessageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor((_) => "O valor preenchido é inválido para este campo.");
+    o.ModelBindingMessageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O valor preenchido é inválido para este campo.");
+    o.ModelBindingMessageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser numérico");
+    o.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor(_ => "O valor preenchido é inválido para este campo");
+    o.ModelBindingMessageProvider.SetValueIsInvalidAccessor((_) => "O valor preenchido é inválido para este campo.");
+    o.ModelBindingMessageProvider.SetValueMustBeANumberAccessor((_) => "O campo deve ser numérico");
+    o.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(_ => "Este campo precisa ser preenchido");
+});
+
+
+builder.Services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+
 
 var app = builder.Build();
 
@@ -54,6 +78,20 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+
+//TODO: DECLARAR ESTE ESCOPO
+var defaultCulture = new CultureInfo("pt-BR");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(defaultCulture),
+    SupportedCultures = new List<CultureInfo> { defaultCulture },
+    SupportedUICultures = new List<CultureInfo> { defaultCulture}
+};
+
+app.UseRequestLocalization(localizationOptions);
+
+
 
 app.MapControllerRoute(
     name: "default",
