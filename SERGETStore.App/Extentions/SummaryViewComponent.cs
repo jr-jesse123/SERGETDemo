42cc252708953b77e404becb2c6103ce86af352e
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-12-27 05:56:15 -0300

    ajustados layouts para bs-5

---
+++ SERGETStore.App/Extentions/SummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SERGETStore.Business.Interfaces;
+
+namespace SERGETStore.App.Extentions
+{
+    public class SummaryViewComponent : ViewComponent
+    {
+        public SummaryViewComponent(INotificador notificador)
+        {
+            Notificador = notificador;
+        }
+
+        public INotificador Notificador { get; }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notificacoes = await Task.FromResult(Notificador.ObterNotificacoes());
+
+            notificacoes.ForEach(n => ViewData.ModelState.AddModelError(String.Empty, n.Mensagem));
+
+
+            return View();
+        }
+    }
+}
